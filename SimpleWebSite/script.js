42d3e888bcec259087332a41352d59f416c1757f
aarontang <aarontang@tencent.com>
2016-04-28 09:42:40 +0800

    [project]init demo(SimpleWebSite from http://www.w3schools.com/website/default.asp, others from self coded from different sites)

---
+++ SimpleWebSite/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by aarontang on 2016/4/25.
+ */
+document.getElementById("foot01").innerHTML =
+    "<p>&copy; " + new Date().getFullYear() + " Creator Company. All Rights Reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+    "<ul id='menu'>" +
+    "<li><a href='index.html'>Home</a></li>" +
+    "<li><a href='customers.html'>Data</a></li>" +
+    "<li><a href='about.html'>About</a></li>"
+    "</ul>"
